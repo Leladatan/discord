b16e6475b891118f989b506553882da891ab8354
Leladatan <vov4ik.samurai@yandex.ru>
2023-09-08 11:48:06 +0300

    create layout for serverId (server sidebar)

---
+++ app/(root)/servers/[serverId]/layout.tsx
@@ -0,0 +1,41 @@
+import currentProfile from "@/utils/current-profile";
+import {redirectToSignIn} from "@clerk/nextjs";
+import {db} from "@/lib/db";
+import {redirect} from "next/navigation";
+import ServerSidebar from "@/components/server/server-sidebar";
+
+const ServerIdLayout = async ({children, params}: {children: React.ReactNode, params: {serverId: string}}) => {
+    const profile = await currentProfile();
+
+    if (!profile) {
+        return redirectToSignIn();
+    }
+
+    const server = await db.server.findUnique({
+        where: {
+            id: params.serverId,
+            members: {
+                some: {
+                    profileId: profile.id
+                }
+            }
+        }
+    });
+
+    if (!server) {
+        return redirect("/servers");
+    }
+
+    return (
+        <div className="h-full">
+            <div className="hidden md:flex h-full w-60 z-20 flex-col fixed inset-y-0">
+                <ServerSidebar serverId={params.serverId} />
+            </div>
+            <main className="h-full md:pl-60">
+                {children}
+            </main>
+        </div>
+    );
+};
+
+export default ServerIdLayout;
