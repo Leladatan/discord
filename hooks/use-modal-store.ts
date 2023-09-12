88c2ddda06a778526bfe2d4c767a944f8dedc7d6
Leladatan <vov4ik.samurai@yandex.ru>
2023-09-12 10:38:17 +0300

    modal store

--- hooks/use-modal-store.ts
+++ hooks/use-modal-store.ts
@@ -1,17 +1,24 @@
 import {create} from "zustand";
+import {Server} from "@prisma/client";
 
-export type ModalType = "createServer";
+export type ModalType = "createServer" | "invite" | "editServer" | "members" | "leaveChannel" | "deleteServer";
+
+interface ModalData {
+    server?: Server;
+}
 
 interface ModalStore {
     type: ModalType | null;
+    data: ModalData;
     isOpen: boolean;
-    onOpen: (type: ModalType) => void;
+    onOpen: (type: ModalType, data?: ModalData) => void;
     onClose: () => void;
 }
 
 export const useModal = create<ModalStore>((set) => ({
     type: null,
+    data: {},
     isOpen: false,
     onClose: () => set({type: null, isOpen: false}),
-    onOpen: (type: ModalType) => set({type, isOpen: true}),
+    onOpen: (type: ModalType, data: ModalData = {}) => set({type, isOpen: true, data}),
 }));
