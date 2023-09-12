29c0c0c067daf292264355c3ca64d8bf00b76b89
Leladatan <vov4ik.samurai@yandex.ru>
2023-09-12 10:38:05 +0300

    servers list

---
+++ app/(root)/servers/components/servers-list.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import {type FC} from 'react';
+import Image from "next/image";
+import {Button} from "@/components/ui/button";
+import {Server} from "@prisma/client";
+import {useRouter} from "next/navigation";
+import {useOrigin} from "@/hooks/use-origin";
+import {AppRouterInstance} from "next/dist/shared/lib/app-router-context";
+
+interface ServersListProps {
+    servers: Server[];
+}
+
+const ServersList: FC<ServersListProps> = ({servers}) => {
+    const origin: string = useOrigin();
+    const router: AppRouterInstance = useRouter();
+
+    return (
+        <div className="grid grid-cols-6 gap-x-20 gap-y-30">
+            {servers.map(server => (
+                <div key={server.id} className="flex flex-col justify-center items-center gap-2">
+                    <div className="w-[100px] h-[100px] relative">
+                        <Image src={server.imageUrl} alt="Server image" fill
+                               className="object-center object-cover rounded-full"/>
+                    </div>
+                    <h2 className="font-bold truncate">
+                        {server.name}
+                    </h2>
+                    <Button
+                        variant="primary"
+                        className="w-full"
+                        onClick={(): void => {
+                            router.push(`${origin}/invite/${server?.inviteCode}`);
+                            router.refresh();
+                        }}
+                    >
+                        Join
+                    </Button>
+                </div>
+            ))}
+        </div>
+    );
+};
+
+export default ServersList;
